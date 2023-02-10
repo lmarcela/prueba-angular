297d16b7f901469bb35eb339eabb4f3aee593944
Marcela Malaver <marcela9409@gmail.com>
2023-02-10 14:25:50 -0500

    ✨ Change registro interface to material

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegistroComponent } from './pages/registro/registro.component';
@@ -8,6 +8,6 @@ import { MaterialModule } from '../utils/material.module';
 
 @NgModule({
   declarations: [RegistroComponent],
-  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, MaterialModule],
+  imports: [CommonModule, AuthRoutingModule, FormsModule, ReactiveFormsModule, MaterialModule],
 })
 export class AuthModule {}
--- src/app/utils/material.module.ts
+++ src/app/utils/material.module.ts
@@ -3,10 +3,31 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatGridListModule } from '@angular/material/grid-list';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatChipsModule } from '@angular/material/chips';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
-  imports: [MatButtonModule, MatGridListModule, MatIconModule, MatTooltipModule, MatFormFieldModule],
-  exports: [MatButtonModule, MatGridListModule, MatIconModule, MatTooltipModule, MatFormFieldModule],
+  imports: [
+    MatButtonModule,
+    MatGridListModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatChipsModule,
+    MatSelectModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatGridListModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatChipsModule,
+    MatSelectModule
+  ],
 })
 export class MaterialModule {}
